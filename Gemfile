29031a4752600a6c1c2937522f6d77e6881329e1
Jeremy Smith <xperts@gmail.com>
2011-10-19 15:42:37 -0400

    cleanup

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'redis', '2.2.2'
 gem 'yajl-ruby', '0.8.3'
 gem 'activesupport', '3.0.10'
 gem 'rubyzip', '0.9.4'
+gem 'require_all', '1.2'
 
 group :test do
 	gem 'cucumber', '1.0.2'
--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,5 @@ require "bundler"
 Bundler.setup
 Bundler.require(:default)
 
-Dir.glob(File.join(File.dirname(__FILE__), 'initializers', "*.rb")){ |file| load file}
-Dir.glob(File.join(File.dirname(__FILE__), '..', 'lib', "*.rb")){ |file| load file}
-Dir.glob(File.join(File.dirname(__FILE__), '../', "*.rb")) { |file| load file}
+require_rel "initializers"
+require_rel "../lib"
